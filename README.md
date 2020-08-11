b1b0613504ed218e50e3805d320853bd3ae57723
zaime ali <zaimeali1997@gmail.com>
2020-08-11 22:34:24 +0500

    Packages Updated

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ npx create-react-app <app-name> --template typescript
 
 
 1) npm i styled-components @types/styled-components
+2) npm install -s react-router-dom
+3) npm i framer-motion
---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// CSS
+import './Loading.css'
+
+export default function Loading() {
+    return (
+        <div>
+            <span className="loadingCircle"></span>
+            <span className="loadingCircle"></span>
+            <span className="loadingCircle"></span>
+        </div>
+    )
+}
